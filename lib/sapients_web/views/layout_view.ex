9d911c6259dc392d77762a6ba3a563f9e58b3919
Kitasio <soulgen@protonmail.com>
2022-08-01 14:35:54 +0300

    made footer

--- lib/sapients_web/views/layout_view.ex
+++ lib/sapients_web/views/layout_view.ex
@@ -4,4 +4,12 @@ defmodule SapientsWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def copyright() do
+    "© #{year()} Sapients Art"
+  end
+
+  def year() do
+    DateTime.utc_now |> Map.fetch!(:year)
+  end
 end
