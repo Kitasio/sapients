374503698015dda75b9fe7bb0c8a1f30760917ab
Kitasio <soulgen@protonmail.com>
2022-08-04 19:31:58 +0300

    lazy load on images in profile

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,11 +24,24 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import Alpine from "alpinejs"
+import intersect from '@alpinejs/intersect'
+ 
+window.Alpine = Alpine
+Alpine.plugin(intersect)
+Alpine.start()
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 
 let liveSocket = new LiveSocket("/live", Socket, {
-  params: {_csrf_token: csrfToken}
+  params: {_csrf_token: csrfToken},
+  dom: {
+    onBeforeElUpdated(from, to) {
+      if (from._x_dataStack) {
+        window.Alpine.clone(from, to)
+      }
+    }
+  }
 })
 
 // Show progress bar on live navigation and form submits
