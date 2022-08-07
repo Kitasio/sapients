7cc1181cab0d644f59a7e7961c39880f31f46212
Kitasio <soulgen@protonmail.com>
2022-08-07 11:38:41 +0300

    funcs for imagekit transforms for page images

--- lib/sapients_web/views/page_view.ex
+++ lib/sapients_web/views/page_view.ex
@@ -10,7 +10,7 @@ defmodule SapientsWeb.PageView do
   def number_of_posts(user) do
     Media.user_posts_amount(user)
   end
-  
+
   def links(user) do
     Media.list_user_links(user)
   end
@@ -19,5 +19,61 @@ defmodule SapientsWeb.PageView do
     uri = link |> URI.parse()
     %URI{host: host} = uri
     host |> String.split(".") |> List.first()
+  end
+
+  defp choose_width(size) do
+    case size do
+      1 ->
+        "tr:w-640"
+      2 ->
+        "tr:w-1024"
+      3 ->
+        "tr:w-1536"
+      _ ->
+        "tr:w-1024"
+    end
+  end
+
+  defp choose_width_index(order) do
+    case order do
+      1 ->
+        "tr:w-1536"
+      _ ->
+        "tr:w-640"
+    end
+  end
+
+  def insert_image_tr(link, size) do
+    uri = link |> URI.parse()
+    %URI{host: host, path: path} = uri
+
+    {filename, list} = path |> String.split("/") |> List.pop_at(-1)
+    bucket = list |> List.last()
+    transformation = choose_width(size)
+
+    case host do
+      "ik.imagekit.io" ->
+        Path.join(["https://", host, bucket, transformation, filename])
+
+      _ ->
+        link
+    end
+  end
+
+  def insert_image_tr_index(link, order) do
+    uri = link |> URI.parse()
+    %URI{host: host, path: path} = uri
+
+    {filename, list} = path |> String.split("/") |> List.pop_at(-1)
+    bucket = list |> List.last()
+    transformation = choose_width_index(order)
+
+    case host do
+      "ik.imagekit.io" ->
+        Path.join(["https://", host, bucket, transformation, filename])
+
+      _ ->
+        link
+    end
   end
 end
