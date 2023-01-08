017f60ef4258791015db69bbdc5869b9ff3556fb
Kitasio <kit.plotits@gmail.com>
2023-01-08 10:09:32 +0200

    name to hex format

--- lib/sapients_web/views/nostr_view.ex
+++ lib/sapients_web/views/nostr_view.ex
@@ -2,10 +2,14 @@ defmodule SapientsWeb.NostrView do
   use SapientsWeb, :view
 
   def render("index.json", %{}) do
+    names = %{
+      langur:
+        "npub1k0x9g3pavpte9ht3mcatedcgyv5005v8mpwwaam7q37a0m3zmguqsxlukk"
+        |> Base.encode16(case: :lower)
+    }
+
     %{
-      names: %{
-        langur: "npub1k0x9g3pavpte9ht3mcatedcgyv5005v8mpwwaam7q37a0m3zmguqsxlukk"
-      }
+      names: names
     }
   end
 end
